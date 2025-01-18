a1b964fb519294fd0bfe02fd218190128ff7f679
Gustavo Cazzine <cazzinengustavo@gmail.com>
2025-01-18 10:27:12 -0300

    att final

--- assets/js/funcoes.js
+++ assets/js/funcoes.js
@@ -1,49 +1,3 @@
-// Carrossel
-const swiper = new Swiper('.swiper', {
-    slidesPerView: 1, // Exibe apenas 1 slide por vez
-    spaceBetween: 10, // Espaço entre os slides
-    loop: true, // Faz o carrossel continuar indefinidamente
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },  
-    autoplay: {
-        delay: 3000, // Tempo em milissegundos (5 segundos)
-        disableOnInteraction: false, // Continua mesmo após interação do usuário
-    },
-});
-
-// Baixar arquivos
-document.addEventListener('DOMContentLoaded', function() {
-    const downloadLinks = document.querySelectorAll('.download-link');
-
-    downloadLinks.forEach(function(link) {
-        link.addEventListener('click', function(event) {
-            // Evita o comportamento padrão do link (evita navegação)
-            event.preventDefault();
-            
-            // Recupera o caminho do PDF do atributo 'data-pdf' do link
-            const pdfPath = link.getAttribute('data-pdf');
-
-            // Criação de um link temporário para o download
-            const a = document.createElement('a');
-            a.href = pdfPath;  // Define o caminho do arquivo PDF
-            a.download = pdfPath.split('/').pop();  // Extrai o nome do arquivo (exemplo: 'documento1.pdf')
-
-            // Simula o clique para iniciar o download
-            document.body.appendChild(a); // Adiciona o link ao DOM temporariamente
-            a.click();  // Simula o clique para download
-
-            // Remove o link temporário após o clique
-            document.body.removeChild(a);
-        });
-    });
-});
-
 // Transição entre slides
 document.addEventListener('DOMContentLoaded', function() {
     const content = document.querySelector('.content');
@@ -122,4 +76,50 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Define o estado inicial
     toggleWhatsAppButton();
+});
+
+// Baixar arquivos
+document.addEventListener('DOMContentLoaded', function() {
+    const downloadLinks = document.querySelectorAll('.download-link');
+
+    downloadLinks.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            // Evita o comportamento padrão do link (evita navegação)
+            event.preventDefault();
+            
+            // Recupera o caminho do PDF do atributo 'data-pdf' do link
+            const pdfPath = link.getAttribute('data-pdf');
+
+            // Criação de um link temporário para o download
+            const a = document.createElement('a');
+            a.href = pdfPath;  // Define o caminho do arquivo PDF
+            a.download = pdfPath.split('/').pop();  // Extrai o nome do arquivo (exemplo: 'documento1.pdf')
+
+            // Simula o clique para iniciar o download
+            document.body.appendChild(a); // Adiciona o link ao DOM temporariamente
+            a.click();  // Simula o clique para download
+
+            // Remove o link temporário após o clique
+            document.body.removeChild(a);
+        });
+    });
+});
+
+// Carrossel
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1, // Exibe apenas 1 slide por vez
+    spaceBetween: 40, // Espaço entre os slides
+    loop: true, // Faz o carrossel continuar indefinidamente
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },  
+    autoplay: {
+        delay: 3000, // Tempo em milissegundos (5 segundos)
+        disableOnInteraction: false, // Continua mesmo após interação do usuário
+    },
 });
